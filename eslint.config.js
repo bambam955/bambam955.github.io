9f2becfbb89a96b192da4dff6e93b41b636434bf
Bennett Moore <bennettmoore05@gmail.com>
2025-01-27 09:16:57 -0500

    use prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
+import eslintConfigPrettierRecommended from "eslint-config-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   ...tseslint.configs.recommended,
+  eslintConfigPrettierRecommended,
 ];
