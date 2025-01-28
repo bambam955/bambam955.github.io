4bcf1230ce4af903cc907361a90d56cddf76e094
Bennett Moore <bennettmoore05@gmail.com>
2025-01-27 21:00:05 -0500

    begin to use prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,16 @@ export default [
     parser: "@typescript-eslint/parser", // Use TypeScript parser
     plugins: [
       "@typescript-eslint", // TypeScript plugin
+      "prettier", // Prettier plugin
     ],
     extends: [
       "eslint:recommended", // Base recommended ESLint rules
       "plugin:@typescript-eslint:recommended", // Additional recommended TypeScript rules
+      "plugin:prettier/recommended", // Add Prettier rules (they won't conflict)
     ],
     rules: {
       // Customize other rules here if needed
+      "prettier/prettier": ["error"], // Enforce Prettier strictly
     }
   }
 ];
