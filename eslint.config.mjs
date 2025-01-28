814078eb46979a7707dcb0f1417cb9a402c46ff5
Bennett Moore <bennettmoore05@gmail.com>
2025-01-27 20:55:07 -0500

    add linting for js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,18 @@ import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.{js,ts,md,mdx}"],
+    parser: "@typescript-eslint/parser", // Use TypeScript parser
+    plugins: [
+      "@typescript-eslint", // TypeScript plugin
+    ],
+    extends: [
+      "eslint:recommended", // Base recommended ESLint rules
+      "plugin:@typescript-eslint:recommended", // Additional recommended TypeScript rules
+    ],
+    rules: {
+      // Customize other rules here if needed
+    }
+  }
 ];
