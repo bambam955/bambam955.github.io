988f8b19d5e0fa274f3877a215981683adef3732
Bennett Moore <bennettmoore05@gmail.com>
2025-01-29 15:35:20 -0500

    try again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import eslintAstroPlugin from 'eslint-plugin-astro';
-import typescriptEslint from 'typescript-eslint';
-import prettierPlugin from 'prettier';
+import typescriptEslintPlugin from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 import eslintJs from '@eslint/js';
 
@@ -8,20 +8,19 @@ export default [
   ...eslintAstroPlugin.configs.recommended,
   {
     ...eslintJs.configs.recommended, // Base recommended ESLint rules
-    ...typescriptEslint.configs.recommended, // Additional recommended TypeScript rules
+    ...typescriptEslintPlugin.configs.recommended, // Additional recommended TypeScript rules
+    ...prettierPlugin.configs.recommended, // Additional Prettier rules (they won't conflict)
     files: ['**/*.{js,ts,md,mdx}'],
     languageOptions: {
       parser: '@typescript-eslint/parser', // Use TypeScript parser
     },
     plugins: {
-      '@typescript-eslint': typescriptEslint, // TypeScript plugin
+      '@typescript-eslint': typescriptEslintPlugin, // TypeScript plugin
       prettier: prettierPlugin, // Prettier plugin
     },
-    extends: [
-      'plugin:prettier/recommended', // Add Prettier rules (they won't conflict)
-    ],
+
+    // Customize other rules here if needed
     rules: {
-      // Customize other rules here if needed
       'prettier/prettier': ['error'], // Enforce Prettier strictly
     },
   },
