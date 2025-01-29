ee839247aa3ddc54d802d9733aaed1fa7314a9f0
Bennett Moore <bennettmoore05@gmail.com>
2025-01-29 15:31:57 -0500

    try another one

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import eslintAstroPlugin from 'eslint-plugin-astro';
-import typescriptEslintPlugin from 'typescript-eslint';
+import typescriptEslint from 'typescript-eslint';
 import prettierPlugin from 'prettier';
 
 import eslintJs from '@eslint/js';
@@ -8,16 +8,16 @@ export default [
   ...eslintAstroPlugin.configs.recommended,
   {
     ...eslintJs.configs.recommended, // Base recommended ESLint rules
+    ...typescriptEslint.configs.recommended, // Additional recommended TypeScript rules
     files: ['**/*.{js,ts,md,mdx}'],
     languageOptions: {
       parser: '@typescript-eslint/parser', // Use TypeScript parser
     },
     plugins: {
-      '@typescript-eslint': typescriptEslintPlugin, // TypeScript plugin
+      '@typescript-eslint': typescriptEslint, // TypeScript plugin
       prettier: prettierPlugin, // Prettier plugin
     },
     extends: [
-      'plugin:@typescript-eslint:recommended', // Additional recommended TypeScript rules
       'plugin:prettier/recommended', // Add Prettier rules (they won't conflict)
     ],
     rules: {
